9c3d1f81d8069dadd4462d2b593b7f440c046b72
Ricy Rifani Putra <ricyrfnputra51@gmail.com>
2026-04-06 21:29:29 +0700

    add selection sort and insertion sort

--- task/sorting/sorting.cpp
+++ task/sorting/sorting.cpp
@@ -3,20 +3,72 @@ using namespace std;
 
 void printArray(int arr[], int n);
 void bubbleSort(int arr[], int n);
+void selectionSort(int arr[], int n);
+void insertionSort(int arr[], int n);
+
 
 int main() {
-    int arr[] = {29, 9, 20, 5, 64};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
+    // Bubble Sort
+    int arrB[] = {29, 9, 20, 5, 64};
+    int sb = sizeof(arrB) / sizeof(arrB[0]);
     cout << "Sebelum Bubble Sort:\n";
-    printArray(arr, n);
-
-    bubbleSort(arr, n);
-
+    printArray(arrB, sb);
+    bubbleSort(arrB, sb);
     cout << "\nSetelah Bubble Sort:\n";
-    printArray(arr, n);
+    printArray(arrB, sb);
+
+    // Selection Sort 
+    int arrS[] = {29, 9, 20, 5, 64};
+    int ss = sizeof(arrS) / sizeof(arrS[0]);
+    cout << "\nss = " << sizeof(arrS) << " / " << sizeof(arrS[0]) << " = " << ss << endl;
+    cout << "\nSebelum Selection Sort\n";
+    printArray(arrS, ss);
+    selectionSort(arrS, ss);
+    cout << "Setelah Selection Sort\n";
+    printArray(arrS, ss);
+
+
+    // Insertion Sort 
+    int arrI[] = {29, 9, 20, 5, 64};  
+    int n = 5;                          
+    cout << "\nSebelum Insertion Sort\n";
+    printArray(arrI, n);
+    insertionSort(arrI, n);
+    cout << "Setelah Insertion Sort\n";
+    printArray(arrI, n);
+
 
     return 0;
+}
+
+void insertionSort(int arr[], int n){  
+    int i, key, j;
+    for(i=1; i<n; i++){
+        key = arr[i];
+        j = i-1;
+
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j-1;
+            printArray(arr, n);     
+        }
+        arr[j+1] = key;
+        printArray(arr, n);        
+    }
+}
+
+void selectionSort(int arr[], int n){
+    int i, j, min_idx, temp;
+    for(i=0; i<n-1; i++){
+        min_idx = i;
+        for(j=i+1; j<n; j++){
+            if(arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
 }
 
 void bubbleSort(int arr[], int n) {
@@ -33,6 +85,7 @@ void bubbleSort(int arr[], int n) {
         printArray(arr, n);
     }
 }
+
 
 void printArray(int arr[], int n) {
     for (int i=0; i<n; i++) {
